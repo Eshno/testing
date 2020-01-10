fd83d02c3067af66fb2bfd0b99130f831f22c057
Eshno <oliver-felix@hotmail.com>
2020-01-10 01:28:11 -0400

    Added small state test

--- __tests__/enzyme/confirmButton.spec.js
+++ __tests__/enzyme/confirmButton.spec.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import { mount } from 'enzyme';
+import { expect } from 'chai';
 import sinon from 'sinon';
 import ConfirmButton from '../../src/components/ConfirmButton';
 
@@ -19,5 +20,13 @@ describe('<ConfirmButton />', function() {
     wrapper.find('.ConfirmButton').simulate('click');
     wrapper.find('.ConfirmButtonCancel').simulate('click');
     sinon.assert.notCalled(callback);
+  });
+
+  it('Changes displayOpts State when Button is clicked', function() {
+    const callback = sinon.spy();
+    const wrapper = mount(<ConfirmButton callback={callback} />);
+    expect(wrapper.state('displayOpts')).to.be.false;
+    wrapper.find('.ConfirmButton').simulate('click');
+    expect(wrapper.state('displayOpts')).to.be.true;
   });
 });
